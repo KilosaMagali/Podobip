0738d3eeb649897bcd162bd1532b849780c793f0
Rabah Meradi <rabahmeradi@gmail.com>
2016-01-28 13:51:16 +0100

    started implementing app service

--- app/src/main/java/students/molecular/podobip/AppService.java
+++ app/src/main/java/students/molecular/podobip/AppService.java
@@ -1,7 +1,19 @@
 package students.molecular.podobip;
 
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
 /**
  * Created by meradi on 28/01/16.
  */
-public class AppService {
+public class AppService extends Service {
+    
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
